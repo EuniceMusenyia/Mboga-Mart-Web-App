cb9c9fb0ade58fc805dd63498e159addf1b69698
clarine <antibaclarine.com>
2023-05-21 15:53:08 +0300

    making changes of the media query

--- file.js
+++ file.js
@@ -1,9 +1,4 @@
-// function toggleMobileMenu(menu) {
-//     menu.classList.toggle('open');
-// }
-const hamburgerMenu = document.querySelector(".hamburger-menu");
-const navMenu = document.querySelector(".nav-menu");
+function toggleMobileMenu(menu) {
+    menu.classList.toggle('open');
+}
 
-hamburgerMenu.addEventListener("click", () => {
-  navMenu.classList.toggle("active");
-});
